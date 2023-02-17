b1b076a1d6437b0ca9d93f598f178dee1510ba67
RuiJ72 <rlagos900@gmail.com>
2023-02-17 17:41:37 -0300

    Adding new relations

--- UniversityApiBackend/Models/DataModels/Category.cs
+++ UniversityApiBackend/Models/DataModels/Category.cs
@@ -4,6 +4,9 @@ namespace UniversityApiBackend.Models.DataModels
 {
     public class Category : BaseEntity
     {
+        [Required]
+        public string Name { get; set; } = string.Empty;
 
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
     }
 }
--- UniversityApiBackend/Models/DataModels/Course.cs
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -21,5 +21,11 @@ namespace UniversityApiBackend.Models.DataModels
         [Required]
         public string Description { get; set; } = string.Empty;
         public Level Level { get; set; } = Level.Basic;
+
+        [Required]
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+
+        [Required]
+        public ICollection<Student> Students { get; set; } = new List<Student>();
     }
 }
